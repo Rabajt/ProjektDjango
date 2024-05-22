7e220c759fbf5997c47e12e5aacbd6ff699a3d22
Łukasz Perkowski <58466856+Rabajt@users.noreply.github.com>
2024-05-22 23:30:28 +0200

    Move all up
    Moving all folders up for jenkins

--- mojastrona/strona/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from .models import Video
-from embed_video.admin import AdminVideoMixin
-
-
-class AdminVideo(AdminVideoMixin, admin.ModelAdmin):
-    pass
-
-admin.site.register(Video, AdminVideo)
