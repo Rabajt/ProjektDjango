803a2e649e70907bebf3bd9824c44cd0ffb80a00
Łukasz Perkowski <58466856+Rabajt@users.noreply.github.com>
2024-05-23 18:47:45 +0200

    All files up
    All files up in hierarchy

--- mojastrona/mojastrona/urls.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', include('strona.urls')),
-    path('members/', include('django.contrib.auth.urls')),
-    path('members/', include('members.urls')),
-]
